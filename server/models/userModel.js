c0e96c25f35590778a0379152377c3da5bde470c
Harsh <harshkumar92200@gmail.com>
2023-11-21 13:06:59 +0530

    Made User Model to store data of user in MongoDB collection

---
+++ server/models/userModel.js
@@ -0,0 +1,32 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        address: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: Number,
+            default: 0
+        }
+    },
+    {timestamps: true},
+);
+
+const User = mongoose.model("USER", userSchema);
+
+export default User;
