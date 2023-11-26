f0a393f07918bc81a85764939e15470be04e8d1a
Harsh <harshkumar92200@gmail.com>
2023-11-26 09:19:16 +0530

    installed cors for cross origin resoucre sharing between server and client

--- server/server.js
+++ server/server.js
@@ -1,11 +1,16 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
+
 import connectDB from "./config/db.js";
 import authRoutes from "./routes/authRoutes.js";
 
 const app = express();
 
 // Middlewares -----------------------
+
+// For Cross Origin Resource Sharing
+app.use(cors());
 
 // To access req.body
 app.use(express.json());
