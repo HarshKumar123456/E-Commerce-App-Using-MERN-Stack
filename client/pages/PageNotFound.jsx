61076c1e1855f73715a852e4141b24022303c893
Harsh <harshkumar92200@gmail.com>
2023-11-27 20:18:59 +0530

    feat: Update PageNotFound component with a custom 404 message and video
    Enhanced the PageNotFound component by incorporating a video background and textual information in separate containers to provide a more engaging 404 error page for users.

--- client/pages/PageNotFound.jsx
+++ client/pages/PageNotFound.jsx
@@ -4,9 +4,20 @@ import Layout from "../components/Layout/Layout";
 const PageNotFound = () => {
     return <>
         <Layout>
-            <h1>
-                This is Page Not Found Page....
-            </h1>
+            <div className="container">
+                <div className="visual-content-container">
+                    <video width="320" height="240" autoPlay muted loop>
+                        <source src="../src/assets/videos/page-not-found.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
+                <div className="textual-container">
+                    <h2>
+                        404 : Page Not Found....
+                    </h2>
+                    <p>Sorry, the page you are looking for is not found....</p>
+                </div>
+            </div>
         </Layout>
     </>;
 };
