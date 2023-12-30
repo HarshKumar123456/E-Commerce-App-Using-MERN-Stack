4be253278a0b61debe409e32c75d3a97cac28c6f
Harsh <harshkumar92200@gmail.com>
2023-12-30 21:54:42 +0530

    Integration: Integrated making payment at client side
    Added Razorpay script in index.html and updated handleCheckout function to make payment if user is logged in

--- client/pages/CartPage.jsx
+++ client/pages/CartPage.jsx
@@ -3,6 +3,8 @@ import Layout from "../components/Layout/Layout";
 import { useCart } from "../context/cart";
 import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../context/auth";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const CartPage = () => {
     const navigate = useNavigate();
@@ -30,10 +32,79 @@ const CartPage = () => {
     };
 
     // Handle Cart Button Click
-    const handleCheckoutButtonClick = () => {
+    const handleCheckoutButtonClick = async (e) => {
         // If User is Logged In Then Proceed To Payment Gateway Portal Else Let the User redirect to the Login Page 
         if (auth?.user) {
-            navigate(`/checkout/payment-page`);
+            // navigate(`/checkout/payment-page`);
+            let amount = getTotalPrice();
+            if (amount === 0) {
+                alert("Oops your cart is empty. Please Shop now....");
+                navigate("/");
+                return;
+            }
+
+            // As in the logic of Razorpay payment amount is taken in paise so convert this amount in paise
+            amount = amount*100;
+            try {
+                const response = await axios.post(`http://localhost:8000/api/v1/payment/createOrder`,
+                    { amount },
+                );
+
+                const order = await response.data;
+                console.log(order);
+
+                if (order.id) {
+                    var options = {
+                        "key": import.meta.env.VITE_RAZORPAY_KEY_ID, // Enter the Key ID generated from the Dashboard
+                        "amount": order.amount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+                        "currency": "INR",
+                        "name": import.meta.env.VITE_BUSINESS_NAME, //your business name
+                        "description": "Payment for Order",
+                        "image": import.meta.env.VITE_BRAND_LOGO_IMAGE_URL,
+                        "order_id": order.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+                        "handler": async function (response) {
+                            alert(response.razorpay_payment_id);
+                            alert(response.razorpay_order_id);
+                            alert(response.razorpay_signature); 
+                        },
+                        "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information, especially their phone number
+                            "name": auth?.user?.name, //your customer's name
+                            "email": auth?.user?.email,
+                            "contact": auth?.user?.phone,  //Provide the customer's phone number for better conversion rates 
+                        },
+                        "notes": {
+                            "address": auth?.user?.address,
+                        },
+                        "theme": {
+                            "color": "#000000",
+                        }
+                    };
+
+                    console.log("We are inside this order.id if condition : ");
+                    var rzp1 = new window.Razorpay(options);
+                    rzp1.on('payment.failed', function (response) {
+                        alert(response.error.code);
+                        alert(response.error.description);
+                        alert(response.error.source);
+                        alert(response.error.step);
+                        alert(response.error.reason);
+                        alert(response.error.metadata.order_id);
+                        alert(response.error.metadata.payment_id);
+                    });
+                    rzp1.open();
+                    e.preventDefault();
+
+                    console.log("Hey Dude mauj aa gayi na bolo bolo haan haan haan....");
+                    toast.success("Order Created Successfully....");
+                } else {
+                    toast.error("Failed to create order....");
+                }
+            } catch (error) {
+                console.error('Error initiating payment:', error);
+                toast.error(error.message);
+            }
+
+
         }
         else {
             navigate(`/login`, {
